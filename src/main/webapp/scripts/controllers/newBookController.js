15c2eef7704b16665a45577e2f6a7c51b159bd31
Karsten Gresch <git-2015@by-doing.org>
2016-11-16 14:31:56 +0100

    Base project after scaffolding

---
+++ src/main/webapp/scripts/controllers/newBookController.js
@@ -0,0 +1,48 @@
+
+angular.module('cdbookstore').controller('NewBookController', function ($scope, $location, locationParser, flash, BookResource , AuthorResource) {
+    $scope.disabled = false;
+    $scope.$location = $location;
+    $scope.book = $scope.book || {};
+    
+    $scope.languageList = [
+        "ENGLISH",
+        "FRENCH",
+        "GERMAN"
+    ];
+    
+    $scope.authorList = AuthorResource.queryAll(function(items){
+        $scope.authorSelectionList = $.map(items, function(item) {
+            return ( {
+                value : item.id,
+                text : item.id
+            });
+        });
+    });
+    $scope.$watch("authorSelection", function(selection) {
+        if ( typeof selection != 'undefined') {
+            $scope.book.author = {};
+            $scope.book.author.id = selection.value;
+        }
+    });
+    
+
+    $scope.save = function() {
+        var successCallback = function(data,responseHeaders){
+            var id = locationParser(responseHeaders);
+            flash.setMessage({'type':'success','text':'The book was created successfully.'});
+            $location.path('/Books');
+        };
+        var errorCallback = function(response) {
+            if(response && response.data) {
+                flash.setMessage({'type': 'error', 'text': response.data.message || response.data}, true);
+            } else {
+                flash.setMessage({'type': 'error', 'text': 'Something broke. Retry, or cancel and start afresh.'}, true);
+            }
+        };
+        BookResource.save($scope.book, successCallback, errorCallback);
+    };
+    
+    $scope.cancel = function() {
+        $location.path("/Books");
+    };
+});
